e80601e13e016504f58ccb2754414e7b1a78a469
dannyherrejon <ricardo_herrejon1@my.vcccd.edu>
2019-09-12 15:39:30 -0700

    i need the best grade possible

--- src/main.cc
+++ src/main.cc
@@ -4,7 +4,7 @@
 /**
  * CS V30 Beginning C++
  * Assignment: EX01-HelloWorld
- *
+ * ;)
  * Statement of code ownership: I hereby state that I have written all of this
  * code and I have not copied this code from any other person or source.
  *
